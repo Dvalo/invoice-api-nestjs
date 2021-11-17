3a27f12ef4045d4ef30edd0b110d897a0862dac0
Dvalo <guram.dvalishvili99@gmail.com>
2021-11-17 18:26:54 +0400

    Invoice creation DTO

--- src/invoices/dto/create-invoice.dto.ts
+++ src/invoices/dto/create-invoice.dto.ts
@@ -1,1 +1,57 @@
-export class CreateInvoiceDto {}
+import {
+  IsInt,
+  IsEmail,
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  IsArray,
+} from 'class-validator';
+import { Address } from '../entities/address.entity';
+import { Item } from '../entities/item.entity';
+
+export class CreateInvoiceDto {
+  @IsString()
+  @IsNotEmpty()
+  createdAt: string;
+
+  @IsString()
+  @IsNotEmpty()
+  paymentDue: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
+  description: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  paymentTerms: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  clientName: string;
+
+  @IsEmail()
+  clientEmail: string;
+
+  @IsString()
+  @IsNotEmpty()
+  status: string;
+
+  @IsNotEmpty()
+  @MinLength(10)
+  SenderAddress: Address;
+
+  @IsNotEmpty()
+  @MinLength(10)
+  clientAddress: Address;
+
+  @IsArray()
+  @IsNotEmpty()
+  items: Item[];
+
+  @IsInt()
+  @IsNotEmpty()
+  total: number;
+}
